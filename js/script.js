52fce97430c3764bd3f401b92541cacc904a5af7
snodgmar000 <snodgmar000@hsestudents.org>
2026-09-01 12:36:51 -0400

    full dark mode
    adds a better dark mode system to the website. Allows for all pages to use it too.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 const music = document.getElementById("music");
 const playBtn = document.getElementById("playBtn");
 
-playBtn.addEventListener("click", function () {
-    if (music.paused) {
-        music.play();
-        playBtn.textContent = "Pause";
-    } else {
-        music.pause();
-        playBtn.textContent = "Play";
-    }
-});
+if (playBtn && music) {
+    playBtn.addEventListener("click", function () {
+        if (music.paused) {
+            music.play();
+            playBtn.textContent = "Pause";
+        } else {
+            music.pause();
+            playBtn.textContent = "Play";
+        }
+    });
+}
 
 const darkBtn = document.getElementById("darkBtn");
-darkBtn.addEventListener("click", function () {
-    document.body.classList.toggle("inverted");
-});
+if (darkBtn) {
+    darkBtn.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+        darkBtn.textContent = document.body.classList.contains("dark-mode")
+            ? "Light Mode"
+            : "Dark Mode";
+    });
+}
